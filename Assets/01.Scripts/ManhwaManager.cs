18ccb750a086c2002a27574ba5b031d8e9e5c49d
Kimjunho06 <kjhlove0119@naver.com>
2024-07-14 07:41:08 +0900

    Manhwa Fade

--- Assets/01.Scripts/ManhwaManager.cs
+++ Assets/01.Scripts/ManhwaManager.cs
@@ -8,11 +8,16 @@ public class ManhwaManager : MonoBehaviour
     public List<Image> images = new List<Image>();
     public int index = 0;
 
+    Image fadeImage;
+
     private void Start()
     {
         foreach (Image image in images)
         {
-            image.gameObject.SetActive(false);
+            Color color = image.color;
+            color.a = 0;
+
+            image.color = color;
         }
     }
 
@@ -23,14 +28,46 @@ public class ManhwaManager : MonoBehaviour
 
             if (index >= images.Count)
             {
+                UIManager.Instance.FadeIn();
                 MCSceneManager.Instance.ChangeScene();
                 return;
             }
 
-            images[index].gameObject.SetActive(true);
+            FadeIn(images[index]);
 
             index++;
             
+        }
+    }
+
+    private Coroutine fadeCoroutine;
+
+    public void FadeIn(Image img)
+    {
+        if (fadeCoroutine != null)
+        {
+            StopCoroutine(fadeCoroutine);
+            
+            Color color = fadeImage.color;
+            color.a = 1;
+
+            fadeImage.color = color;
+        }
+        fadeCoroutine = null;
+
+        fadeCoroutine = StartCoroutine(FadeInCoroutine(img));
+    }
+
+    private IEnumerator FadeInCoroutine(Image img)
+    {
+        while (img.color.a < 1)
+        {
+            fadeImage = img;
+
+            Color color = img.color;
+            color.a += Time.deltaTime;
+            img.color = color;
+            yield return null;
         }
     }
 }
